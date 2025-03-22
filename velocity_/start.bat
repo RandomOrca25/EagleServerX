19d8c6e492ca3e6849ef891471dc85321cb54355
RandomOrca25 <ayanchowhan13@gmail.com>
2025-03-21 19:20:05 -0700

    finished velocity file uploads
    nerdy stuff dw about it :]

---
+++ velocity_/start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity-3.4.0-SNAPSHOT-480.jar
+pause
